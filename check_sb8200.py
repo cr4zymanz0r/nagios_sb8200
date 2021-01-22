81778bc840cdbd662ea27504ba54fd528482e150
cr4zymanz0r <>
2021-01-22 16:14:13 -0600

    Update to Python3 + attempt to fail gracefully

--- check_sb8200.py
+++ check_sb8200.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -7,7 +7,7 @@ import os.path
 import requests
 import base64
 import json
-import urllib2
+import urllib.request
 import urllib3
 import sys
 import mysql.connector
@@ -25,10 +25,10 @@ conn_type = config['conn_type']
 if (conn_type == 'http'):
 	url = 'http://192.168.100.1/cmconnectionstatus.html'
 	try:
-			html = urllib2.urlopen(url).read()
+		html = urllib.request.urlopen(url).read()
 	except:
-			print "WARNING: Unable to read from HTTP URL"
-			exit(1)
+		print "WARNING: Unable to read from HTTP URL"
+		exit(1)
 elif (conn_type == 'https'):
 	username = config['username']
 	password = config['password']
@@ -43,26 +43,25 @@ elif (conn_type == 'https'):
 	session1.headers.update({'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'})
 	session1.headers.update({'Authorization': 'Basic ' + cm_cred})
 	
-	request1 = session1.get('https://192.168.100.1/cmconnectionstatus.html?' + cm_cred, verify=False)
-	
-	session2 = requests.Session()
-	session2.headers.update({'Cookie': 'HttpOnly: true, Secure: true; credential=' + request1.text})
 	try:
-			request2 = session2.get('https://192.168.100.1/cmconnectionstatus.html', verify=False)
-			html = request2.text
+		request1 = session1.get('https://192.168.100.1/cmconnectionstatus.html?' + cm_cred, verify=False)
+		session2 = requests.Session()
+		session2.headers.update({'Cookie': 'HttpOnly: true, Secure: true; credential=' + request1.text})
+
+		request2 = session2.get('https://192.168.100.1/cmconnectionstatus.html', verify=False)
+		html = request2.text
 	except:
-			print "WARNING: Unable to read from HTTPS URL"
-			exit(0) #was 1, set to 0 so docker image won't quit on failure
+		print "WARNING: Unable to read from HTTPS URL"
+		exit(0) #was 1, set to 0 so docker image won't quit on failure
 	
 	session3 = requests.Session()
 	try:
-			session3.get('https://192.168.100.1/logout.html', verify=False)
+		session3.get('https://192.168.100.1/logout.html', verify=False)
 	except:
-			pass
+		pass
 		
 soup = BeautifulSoup(html, 'lxml')
 table = soup.find_all('table')
-	
 
 # Define sections
 
@@ -103,19 +102,19 @@ if sql:
 # Status
 
 for i in range(len(startDetail)):
-        startData = startDetail[i].find_all('td')
-        startProc = startData[0].string
-        startStat = startData[1].string
-        #startComm = startData[2]
-        if startStat == "OK":
-                status = "OK:"
-        elif startStat == "Enabled":
-                status = "OK:"
-        elif startStat == "Allowed":
-                status = "OK:"
-        else:
-                status = "CRITICAL:"
-                break
+	startData = startDetail[i].find_all('td')
+	startProc = startData[0].string
+	startStat = startData[1].string
+	#startComm = startData[2]
+	if startStat == "OK":
+		status = "OK:"
+	elif startStat == "Enabled":
+		status = "OK:"
+	elif startStat == "Allowed":
+		status = "OK:"
+	else:
+		status = "CRITICAL:"
+		break
 if not sql:
 	print status, startProc, startStat,
 	print "|",
@@ -123,44 +122,45 @@ if not sql:
 # Perfdata
 
 for i in range(len(downChannel)):
-        dChData = downChannel[i].find_all('td')
-        dChNum = dChData[0].string
-        dChPow = dChData[4].string.split()
-        dChSNR = dChData[5].string.split()
-        dChCorr = dChData[6].string
-        dChUncorr = dChData[7].string
+	dChData = downChannel[i].find_all('td')
+	dChNum = dChData[0].string
+	dChPow = dChData[4].string.split()
+	dChSNR = dChData[5].string.split()
+	dChCorr = dChData[6].string
+	dChUncorr = dChData[7].string
+
 	if not sql:
-        	print "d_" + dChNum + "_pow=" + dChPow[0],
-        	print "d_" + dChNum + "_snr=" + dChSNR[0],
-        	print "d_" + dChNum + "_corr=" + dChCorr,
-        	print "d_" + dChNum + "_uncorr=" + dChUncorr,
+		print "d_" + dChNum + "_pow=" + dChPow[0],
+		print "d_" + dChNum + "_snr=" + dChSNR[0],
+		print "d_" + dChNum + "_corr=" + dChCorr,
+		print "d_" + dChNum + "_uncorr=" + dChUncorr,
 	else:
 		#print "downlink_channel,interface=" + str(i) + " value="+dChNum
 		#print "downlink_power,interface=" + str(i) + " value="+dChPow[0]
-                #print "downlink_snr,interface=" + str(i) + " value="+ dChSNR[0]
-                #print "downlink_corrected,interface=" + str(i) + " value="+ dChCorr
+		#print "downlink_snr,interface=" + str(i) + " value="+ dChSNR[0]
+		#print "downlink_corrected,interface=" + str(i) + " value="+ dChCorr
 		#print "downlink_uncorrected,interface=" + str(i) + " value="+dChUncorr
 		downlink_data = (dChNum, dChPow[0], dChSNR[0], dChCorr, dChUncorr, timenow)
 		cursor.execute(add_downlink, downlink_data)
 		
 for i in range(len(upChannel)):
 	uChData = upChannel[i].find_all('td')
-        uChNum = uChData[1].string
-        uChPow = uChData[6].string.split()
-        if not sql:
-        	print "u_" + uChNum + "_pow=" + uChPow[0],
+	uChNum = uChData[1].string
+	uChPow = uChData[6].string.split()
+	if not sql:
+		print "u_" + uChNum + "_pow=" + uChPow[0],
 	else:
 		#print "uplink_channel,interface="+ str(i) + " value=" + uChNum
-                #print "uplink_power,interface="+ str(i) + " value=" + uChPow[0]
+		#print "uplink_power,interface="+ str(i) + " value=" + uChPow[0]
 		uplink_data = (uChNum, uChPow[0], timenow)
 		cursor.execute(add_uplink, uplink_data)
 # Exit Statuses
 
 if status == "OK:":
-		if sql:
-			sqlconnection.commit()
-			sqlconnection.close()
-			
-		exit(0)
+	if sql:
+		sqlconnection.commit()
+		sqlconnection.close()
+		
+	exit(0)
 else:
 	exit(2)
--- install-sb8200.sh
+++ install-sb8200.sh
@@ -2,5 +2,5 @@
 
 apt-get update
 
-apt-get install python python-pip -y
-pip install beautifulsoup4 requests mysql-connector-python lxml
+apt-get install python3 python3-pip -y
+pip3 install beautifulsoup4 requests mysql-connector-python lxml
